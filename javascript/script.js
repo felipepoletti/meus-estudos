30b6fbbd16ac0e78f3b753bea831ff353c9b7a38
Felipe Poletti <felipepolettig@gmail.com>
2023-05-16 20:49:01 -0300

    tipos de dados em javascript

--- javascript/script.js
+++ javascript/script.js
@@ -11,3 +11,50 @@ const z = 20;
 /*
   Em constantes não é possível reatribuir valores, em let sim.- 
 */
+
+// tipos de dados
+const name = 'Felipe Poletti'; // text
+console.log(name);
+console.log(typeof name);
+
+const age = 22; // number
+console.log(age);
+console.log(typeof age);
+
+/* 
+ javascript não possui diferenciação entre números
+ a variável abaixo também é considerada number
+*/
+
+const decimal = 13.5; // number
+console.log(decimal);
+console.log(typeof decimal);
+
+const isApproved = true; // bool (true ou false)
+console.log(isApproved);
+console.log(typeof isApproved);
+
+let surname = null; // null (object)
+console.log(surname);
+console.log(typeof surname);
+
+// muitos dados em javascript são considerado objetos, null é um deles
+
+let undefined // undefined
+console.log(undefined);
+console.log(typeof undefined);
+
+const languages = ['Javascript', 'C#', 'Python']; // arrays
+console.log(languages);
+console.log(typeof languages);
+
+// arrays também são considerados objetos pelo javascript
+
+const user = { //object
+  email: 'felipepolettig@gmail.com', 
+  password: 'P@ssw0rd', 
+  username: 'felipepoletti', 
+  age: 22
+}
+console.log(user);
+console.log(typeof user);
