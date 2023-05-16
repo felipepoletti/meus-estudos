b7a94a4fe025345eec5f8c468fb8489c64e1d0f9
Felipe Poletti <felipepolettig@gmail.com>
2023-05-16 20:35:14 -0300

    variables in javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,13 @@
-console.log("This is a message in javascript");
-console.error("This is an error in javascript");
-console.warn("This is a warning in javascript");
+// console
+console.log('This is a message in javascript');
+console.error('This is an error in javascript');
+console.warn('This is a warning in javascript');
+
+// variáveis
+var x = 10; // não se usa mais
+let y = 15;
+const z = 20;
+
+/*
+  Em constantes não é possível reatribuir valores, em let sim.- 
+*/
